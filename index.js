bcdfe1167da20a13e49ce24c759bf3ac68fea2cc
Ben McShane <benjaminmartinmc@gmail.com>
2022-08-01 14:19:27 -0600

    commit tree 9670a36513a32f6af3e9432b3259ea52cb59dc58

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string) {
+   if (console.log(string === string.toLowerCase()) === true) {
+    string = 'I can\'t hear you!'
+   } 
+   if (console.log(string === string.toUpperCase()) === true) {
+    string = 'YES INDEED!'
+   } 
+   if(console.log(string === string.toLowerCase) || console.log(string === string.toUpperCase)) {
+    string = 'Let\'s have dinner together!'
+   }
+   return string
+ 
+  
+}
